64e77b80173d2606a0866e7b963be903680be26f
Tristan Agaesse <tristan.agaesse@gmail.com>
2023-03-26 12:38:42 +0200

    backend: load .env in backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { DatabaseModule } from './database/database.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OfferModule } from './offer/offer.module';
 
 @Module({
-  imports: [OfferModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    OfferModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
