32714db8c7fb5120ce82bb2aa2e9263bf6221ae1
Tristan Agaesse <tristan.agaesse@gmail.com>
2023-04-02 21:30:04 +0200

    front: clean up code of liquidation code

--- webapp/src/pages/liquidations.tsx
+++ webapp/src/pages/liquidations.tsx
@@ -5,31 +5,18 @@ import {
   BaseView,
   ContractCallButton,
 } from "components/BaseComponents";
-import ListLoader, { MakeListItemProps } from "components/DataLoaders";
+import ListLoader from "components/DataLoaders";
 import {
   FullLoanInfo,
   getFullLoanInfo,
   getLoanIds,
   getSupplyABI,
   getSupplyAddress,
-  getSupplyTokenAddresses,
-  getTokenBalance,
-  getTroveManagerABI,
-  getTroveManagerAddress,
-  TokenBalanceInfo,
 } from "libs/unilend_utils";
-import { MakeOfferInputs } from "components/InputViews";
-import { LoanView, OfferView, TokenBalanceView } from "components/DataViews";
+import { LoanView } from "components/DataViews";
 import { eventEmitter, EventType } from "libs/eventEmitter";
-import {
-  FullOfferInfo,
-  getOfferKey,
-  getOffersFrom,
-  offerRevoked,
-} from "libs/backend";
 import { Flex, Spacer } from "@chakra-ui/react";
 import { BigNumber } from "ethers";
-import { ReactNode } from "react";
 
 export default function LiquidationsPage() {
   const { address: account, isConnecting, isDisconnected } = useAccount();
@@ -41,61 +28,6 @@ export default function LiquidationsPage() {
       isDisconnected={isDisconnected}
     >
       <VStack align="left" spacing="4">
-        <Box>
-          <Heading as="h6" size="sm" mb="3">
-            {"Loans"}
-          </Heading>
-          <ListLoader
-            fetchData={() => getOffersFrom(provider, account!)}
-            reloadEvents={[
-              { eventType: EventType.SUPPLY_OFFER_CREATED },
-              { eventType: EventType.SUPPLY_OFFER_CANCELLED },
-            ]}
-            makeListItem={(props) => {
-              console.log("list item: " + props);
-              return (
-                <BaseView
-                  fetcher={() => Promise.resolve(props.id)}
-                  level={2}
-                  key={getOfferKey(props.id.offer)}
-                  dataView={(data: FullOfferInfo) => {
-                    return <OfferView data={data}></OfferView>;
-                  }}
-                  actions={[
-                    {
-                      action: "Revoke",
-                      onClickView: (
-                        data: FullOfferInfo,
-                        actionFinished: () => any
-                      ) => {
-                        return (
-                          <Flex w="100%">
-                            <Spacer></Spacer>
-                            <ContractCallButton
-                              contractAddress={getSupplyAddress()}
-                              abi={getSupplyABI()}
-                              functionName={"setOfferNonce"}
-                              args={[data.offer.offerId, 1]}
-                              enabled={true}
-                              callback={() => {
-                                actionFinished();
-                                offerRevoked(data);
-                                eventEmitter.dispatch({
-                                  eventType: EventType.SUPPLY_OFFER_CANCELLED,
-                                });
-                              }}
-                            ></ContractCallButton>
-                          </Flex>
-                        );
-                      },
-                    },
-                  ]}
-                ></BaseView>
-              );
-            }}
-          ></ListLoader>
-        </Box>
-
         <Box>
           <Heading as="h6" size="sm" mb="3">
             {"Loans"}
