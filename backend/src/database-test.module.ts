9d0f6cc56b566f7030f0cf097ba143916f10cd9b
Tristan Agaesse <tristan.agaesse@gmail.com>
2023-04-19 11:29:31 +0200

    backend: fix connection test module

--- backend/src/database-test.module.ts
+++ backend/src/database-test.module.ts
@@ -1,16 +1,27 @@
 import { Module, OnModuleInit } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { getConnection } from 'typeorm';
+import { createConnection } from 'typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    ConfigModule.forRoot(),
   ],
 })
 export class DatabaseTestModule implements OnModuleInit {
+  constructor(private readonly configService: ConfigService) {}
+
   async onModuleInit() {
     try {
-      const connection = getConnection();
+      const connection = await createConnection({
+        type: 'postgres',
+        host: this.configService.get('DB_HOST'),
+        port: parseInt(this.configService.get('DB_PORT') as string, 10),
+        username: this.configService.get('DB_USERNAME'),
+        password: this.configService.get('DB_PASSWORD'),
+        database: this.configService.get('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // Use false in production: this creates tables and columns automatically
+      });
       console.log('Database connection successful');
     } catch (error) {
       console.error('Error connecting to the database:', error);
